4c277d637e62b6f3806b7dca60ffdea080192b33
anjaligupta235490-lang <anjaligupta235490@gmail.com>
2026-09-01 19:08:30 +0530

    feat: add Find All Numbers Disappeared in an Array solution

--- algorithms/find-all-numbers-disappeared-in-an-array/solution.py
+++ algorithms/find-all-numbers-disappeared-in-an-array/solution.py
@@ -5,20 +5,27 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(nums)
-        i = 0
-        while i < n:
-            correct = nums[i] -1
-            if nums[i] != nums[correct]:
-                nums[i], nums[correct] = nums[correct], nums[i]
-            else:
-                i += 1
+        # i = 0
+        # while i < n:
+        #     correct = nums[i] -1
+        #     if nums[i] != nums[correct]:
+        #         nums[i], nums[correct] = nums[correct], nums[i]
+        #     else:
+        #         i += 1
 
+        # arr = []
+
+        # for i in range(n):
+        #     if nums[i] != i+1:
+        #         arr.append(i+1)
+        # return arr
+        s = set(nums)
         arr = []
-
-        for i in range(n):
-            if nums[i] != i+1:
-                arr.append(i+1)
+        for i in range(1,n+1):
+            if i not in s:
+                arr.append(i)
         return arr
+
 
 
         
