ba4afec4a4b0d6c96309348ea8901dda87a8fbff
anjaligupta235490-lang <anjaligupta235490@gmail.com>
2026-09-03 18:56:12 +0530

    feat: add Sqrt(x) solution

--- algorithms/sqrtx/solution.py
+++ algorithms/sqrtx/solution.py
@@ -4,9 +4,19 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        i = 1
-        while i*i <= x:
-            i += 1
-        return i-1
-
+        if x == 0:
+            return 0
+        left = 1
+        right = x
+        ans = 0
+        while left <= right:
+            mid = (left + right)//2
+            if mid*mid == x:
+                return mid
+            elif mid*mid < x:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
         
