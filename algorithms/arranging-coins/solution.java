b3033c99594b3094298ba591318b9746ce6e468a
anjaligupta235490-lang <anjaligupta235490@gmail.com>
2026-09-08 03:06:52 +0530

    feat: add Arranging Coins solution

---
+++ algorithms/arranging-coins/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0){
+            return false;
+        }
+        int[] factors = {2,3,5};
+        for(int i : factors){
+            while(n%i == 0){
+                n = n/i;
+            }
+        }
+        return n == 1;
+        
+    }
+}
