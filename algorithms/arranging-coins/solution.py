e1599347364ff690ff96798ea45b8ea7e6ca8e45
anjaligupta235490-lang <anjaligupta235490@gmail.com>
2026-09-08 17:38:27 +0530

    feat: add Arranging Coins solution

--- algorithms/arranging-coins/solution.py
+++ algorithms/arranging-coins/solution.py
@@ -4,14 +4,25 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        row = 0
-        available = n
-        required = 1
-        while available >= required:
-            available -= required
-            row += 1
-            required += 1
-        return row
+        # row = 0
+        # available = n
+        # required = 1
+        # while available >= required:
+        #     available -= required
+        #     row += 1
+        #     required += 1
+        # return row
+        low = 1
+        high = n
+        while low <= high:
+            mid = (low + high)//2
+            required = mid*(mid+1)//2
+            if required <= n:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return high
+
 
 
 
